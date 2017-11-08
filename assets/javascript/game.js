2a01f1c04375060a63e5564a3b634b301b584f90
William Robertson <bizarrowill@MacBook-Pro.local>
2017-11-08 00:49:19 -0700

    updates to notes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,29 @@
-// Define alphabet letters that computer can pick from
+// define computer options in alphabet
 var alphabetLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// Set the initial global variables
+// establish initial variables
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
 // guessesSoFar is an array that will hold all the user's guesses in each round
 var guessesSoFar = [];
-// userGuess is what the user picks by pressing a key
+// userGuess by pressing a key
 var userGuess = null;
-// Have computer pick a letter and store it in letterToBeGuessed
+// computer pick a letter and store it in letterToBeGuessed
 var letterToBeGuessed = alphabetLetters[Math.floor(Math.random() * alphabetLetters.length)];
 console.log("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + guessesLeft + " Guesses so far: " + guessesSoFar + " Computer picked: " + letterToBeGuessed);
 
-// start listening for events
+// game events
 document.onkeyup = function(event) {
 
 	// When user presses a key, it records it and saves to userGuess
 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
 	// Add the user's guess to guessesSoFar array but 
-	// only if it wasn't already previously picked by the user
-	// also make sure that the character user picks is within
-	// the alphabet, and not any non-usable character
+	// only if it has not been picked, and is within
+	// the alphabet, not special character
 	if (guessesSoFar.indexOf(userGuess) < 0 && alphabetLetters.indexOf(userGuess) >= 0) {
 		guessesSoFar[guessesSoFar.length]=userGuess;
-		// if it is a new letter then decrease remaining guesses by 1
+		// if it is a new letter, decrease guessesLeft by 1
 		guessesLeft--;
 	}
 
@@ -53,7 +52,11 @@ document.onkeyup = function(event) {
 	}
 
 	// Displaying progress to HTML
-	var html = "<p><h1>The Psychic Game</h1></p>" + "<p><h4>Guess what letter I\'m thinking of</h4></p>" + "<p><h4>Wins: " + wins + "</h4></p>" + "<p><h4>Losses: " + losses + "</h4></p>" + "<p><h4>Guesses Left: " + guessesLeft + "</h4></p>" + "<p><h4>Your guesses so far: " + guessesSoFar + "</h4></p>";
+	var html = "<p><h1>The Psychic Game</h1></p>" + "<p><h4>Guess what letter I'm thinking of</h4></p>" + "<p><h4>Wins: " + wins + "</h4></p>" + "<p><h4>Losses: " + losses + "</h4></p>" + "<p><h4>Guesses Left: " + guessesLeft + "</h4></p>" + "<p><h4>Your guesses so far: " + guessesSoFar + "</h4></p>";
+	
+	
+	//testing below
+	
 	// place html into the game ID
 	document.querySelector("#game").innerHTML = html;
 
